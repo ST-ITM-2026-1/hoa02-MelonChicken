4bd260dcebfc250dc54eaa261c74f380de172115
JUNU <m.l02n23@gmail.com>
2026-04-15 22:00:39 +0900

    feat (js): improve to open only one dropdown for clear ui

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", init);
 
+let currentFilterdict = {
+    category: [],
+    status: []
+};
+
 function init() {
     // console.log("Lets start");
     let dropdownContents = document.querySelectorAll(".dropdownContent label");
@@ -11,30 +16,60 @@ function init() {
     })
 }
 
-function dropdownClick(content) {
-    console.log(content);
+function dropdownClick(clickedElement) {
+    let content = clickedElement.target;
+    // console.log(content);
     // How to use .classList
     // https://developer.mozilla.org/ko/docs/Web/API/Element/classList
-    if (!content.target.classList.contains("dropdown")) {
-        console.log(content.target.parentElement);
-        dropdownContent = content.target.parentElement.getElementsByClassName("dropdownContent");
-    } else {
-        console.log(`It is ${content}`);
-        dropdownContent = content.target.getElementsByClassName("dropdownContent");
+
+    while (!content.classList.contains("dropdown")) {
+        content = content.parentElement;
     }
-    if (dropdownContent[0].style.display == "None") {
-        dropdownContent[0].style.display = "grid";
-    } else if (dropdownContent[0].style.display == "grid") {
+    // console.log(`It is ${content}`);
+    let dropdownContent = content.getElementsByClassName("dropdownContent");
+    let dropdownContents = document.querySelectorAll(".dropdownContent")
+    let isOpen = dropdownContent[0].classList.contains("open");
+    dropdownContents.forEach(content => {
+        content.classList.remove("open");
+        content.style.display = "None";
+    });
+    // console.log(dropdownContent);
+    if (isOpen) {
         dropdownContent[0].style.display = "None";
     } else {
         dropdownContent[0].style.display = "grid";
+        dropdownContent[0].classList.add("open");
     }
+
 }
 
 
 /**
  * function to load the filtered contents
  */
-function filterClick() {
+function filterClick(clickedElement) {
+    let content = clickedElement.target;
+    // ignore double-click which happens due to the label? I think so... maybe
+    if (content.tagName != "INPUT") {
+        return;
+    }
 
+    if (content.classList.contains("checked")) {
+        content.classList.remove("checked");
+    }
+    else {
+        content.classList.add("checked");
+    }
+    applyToList(content.name, content.id);
+}
+
+function applyToList(name, id) {
+    let filterlist = currentFilterdict[name];
+    if (filterlist.includes(id)) {
+        filterlist.splice(filterlist.indexOf(id));
+    }
+
+    else {
+        filterlist.push(id);
+    }
 }
